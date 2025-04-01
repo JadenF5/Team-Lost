d83be4e4bd69510c82c10b0763c43236945e5a6d
harishan623 <harishanamirthanathan@gmail.com>
2025-04-01 11:24:15 -0400

    initial commit

---
+++ frontend/main-script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchBar = document.getElementById("search-bar");
+    const searchButton = document.getElementById("search-button");
+
+    const leftArrows = document.querySelectorAll(".slider-arrow.left");
+    const rightArrows = document.querySelectorAll(".slider-arrow.right");
+    const eventRows = document.querySelectorAll(".event-row");
+
+    // Add event listeners for left arrows
+    leftArrows.forEach((arrow) => {
+        arrow.addEventListener("click", function () {
+            const row = this.nextElementSibling;
+            const cardWidth = row.querySelector(".event-card").offsetWidth + 20;
+            row.scrollBy({
+                left: -cardWidth * 1,
+                behavior: "smooth",
+            });
+        });
+    });
+
+    rightArrows.forEach((arrow) => {
+        arrow.addEventListener("click", function () {
+            const row = this.previousElementSibling;
+            const cardWidth = row.querySelector(".event-card").offsetWidth + 20;
+            row.scrollBy({
+                left: cardWidth * 1,
+                behavior: "smooth",
+            });
+        });
+    });
+
+    updateArrowVisibility();
+
+    eventRows.forEach((row) => {
+        row.addEventListener("scroll", function () {
+            updateArrowVisibility();
+        });
+    });
+
+    window.addEventListener("resize", updateArrowVisibility);
+
+    function updateArrowVisibility() {
+        eventRows.forEach((row) => {
+            const leftArrow = row.previousElementSibling;
+            const rightArrow = row.nextElementSibling;
+
+            leftArrow.style.opacity = row.scrollLeft > 20 ? "1" : "0.3";
+
+            const isAtEnd =
+                row.scrollLeft + row.clientWidth >= row.scrollWidth - 20;
+            rightArrow.style.opacity = isAtEnd ? "0.3" : "1";
+        });
+    }
+});
---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.querySelector(".login-btn");
+
+    loginButton.addEventListener("click", function () {
+        window.location.href = "main.html";
+    });
+});
